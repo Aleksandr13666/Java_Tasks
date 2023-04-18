2a6fe8422ba5db0d3899f4e811d541d31412bab2
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-04-18 11:22:19 +0700

    added task2

--- src/main/java/com/example/seminar1/cw2/cw2.java
+++ src/main/java/com/example/seminar1/cw2/cw2.java
@@ -6,7 +6,28 @@ public class cw2 {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
-    
     scanner.close();
+    System.out.println(countDiff(n, scanner));
+    }
+
+    /**
+     * 
+     * @param n
+     * @param scanner
+     * @return
+     */
+
+    public static int countDiff(int n, Scanner scanner){
+        int count = 0;
+    int a = scanner.nextInt();
+    for (int i = 0; i < n - 1; i++) {
+        int b = scanner.nextInt();
+        if (a > 0 && b < 0){
+            count++;
+        }
+        a = b;
+    }
+    scanner.close();
+    return count;
     }
 }
