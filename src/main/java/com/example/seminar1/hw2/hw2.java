4e938ba0c72088214ddf441f877b6dbaa51ba4a0
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-05-04 08:22:24 +0700

    Complete hw1

---
+++ src/main/java/com/example/seminar1/hw2/hw2.java
@@ -0,0 +1,18 @@
+package com.example.seminar1.hw2;
+
+import java.util.Scanner;
+public class hw2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+
+        while(true){
+            int x = sc.nextInt();
+            if (x != 0) n += x;
+            else break;;
+
+        }
+        System.out.println(n);
+        sc.close();
+    }
+}
