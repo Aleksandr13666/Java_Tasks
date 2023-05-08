e610d40c4527ae06f1f5c9fab244509193a1d2a1
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-05-08 17:16:27 +0700

    Complete hw2 seminar2

---
+++ src/main/java/com/example/seminar2/hw2/hw2.java
@@ -0,0 +1,26 @@
+package com.example.seminar2.hw2;
+
+import java.util.Scanner;
+public class hw2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantity of numbers: ");
+        System.out.println();
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            System.out.println("Input a number: ");
+            array[i] = sc.nextInt();
+        }
+        boolean isSorted = array[0] < array[1];
+        for (int i = 2; i < array.length ; i++) {
+            if ( array[i - 1] <= array[i] && !isSorted){
+                System.out.println("Not sorted.");
+                return;
+            }
+
+        }
+        System.out.println(isSorted? "Is sorted" : "Not sorted");
+    }
+
+}
